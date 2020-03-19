31c817b243930675eb5dbab39cbc95278880d36d
Zach Wegner <zwegner@gmail.com>
2020-03-19 14:04:26 -0500

    Add init.py script to set up new machine with symlinks etc

---
+++ init.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+
+BASE = os.path.abspath(sys.path[0])
+HOME = os.path.expanduser('~')
+
+def symlink(dir, blacklist={}):
+    src = os.path.abspath('%s/%s' % (BASE, dir))
+    dst = os.path.abspath('%s/%s' % (HOME, dir))
+    for f in os.listdir(src):
+        if f in blacklist:
+            continue
+        src_path = '%s/%s' % (src, f)
+        cmd = ['ln', '-s', '-T', src_path, '%s/%s' % (dst, f)]
+        subprocess.run(cmd)
+
+symlink('.', {'.git', 'init.py', '.config', '.nvim'})
+symlink('.config')
+symlink('.nvim')
+
+for d in ['backup', 'swap', 'undo']:
+    os.makedirs('%s/.nvim/%s' % (HOME, d), exist_ok=True)
